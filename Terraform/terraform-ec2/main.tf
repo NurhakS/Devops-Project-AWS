13946dcb9c9b32cdf3d1059d5d1c74b73140b49a
Nurhaks <nicksozer@gmail.com>
2024-11-13 13:32:39 +0300

    Add new files or folder

---
+++ Terraform/terraform-ec2/main.tf
@@ -0,0 +1,41 @@
+# Provider configuration
+provider "aws" {
+  region = "us-east-1" # Specify the region
+}
+
+# Create a new security group that allows all inbound and outbound traffic
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all_traffic"
+  description = "Security group that allows all inbound and outbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Launch an EC2 instance
+resource "aws_instance" "my_ec2_instance" {
+  ami             = "ami-0325498274077fac5"
+  instance_type   = "t4g.medium"
+  key_name        = "ec2key"
+  security_groups = [aws_security_group.allow_all.name]
+
+  # Configure root block device
+  root_block_device {
+    volume_size = 30
+  }
+
+  tags = {
+    Name = "MyUbuntuInstance"
+  }
+}
